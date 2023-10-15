ec0a4e9ce273e272df7392092ccc25a3f11ab867
Joao <joaocarlosfernandes14@gmail.com>
2023-10-15 21:59:27 +0100

    Everything done

--- mod1/cnn.py
+++ mod1/cnn.py
@@ -10,7 +10,7 @@ class CNN(nn.Module):
         self.conv1 = nn.Conv2d(1, 6, kernel_size=5) # Criacao da primeira convolutional layer
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(6, 16, kernel_size=5)  # Criacao da segunda convolutional layer
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2) # Criacao da terceira convolutional layer
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.fc1 = nn.Linear(16 * 4 * 4, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10) 
